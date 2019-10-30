b91a45c37e0263892475528480d87f66688c192c
alefreis <alef.reis@linkapi.com.br>
2019-10-29 22:22:01 -0300

    update

--- app/src/main/java/br/com/leiaaqui/ClienteController.java
+++ app/src/main/java/br/com/leiaaqui/ClienteController.java
@@ -1,8 +1,10 @@
 package br.com.leiaaqui;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.ContentValues;
 import android.content.Context;
+import android.provider.ContactsContract;
 
 public class ClienteController {
     private SQLiteDatabase db;
@@ -37,5 +39,75 @@ public class ClienteController {
         } else {
             return "Registro inserido com sucesso";
         }
+    }
+
+    public Cursor list() {
+        Cursor cursor;
+
+        String[] campos = {DatabaseManager.getIdClientes(), DatabaseManager.getNomeClientes(),
+                DatabaseManager.getEnderecoClientes(), DatabaseManager.getEmailClientes(),
+                DatabaseManager.getCelularClientes(), DatabaseManager.getCpfClientes(),
+                DatabaseManager.getDtNascimentoClientes()};
+
+        db = banco.getReadableDatabase();
+        cursor = db.query(banco.getTabelaClientes(), campos,
+                null, null, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public Cursor findOne(int id) {
+        Cursor cursor;
+
+        String[] campos = {DatabaseManager.getIdClientes(), DatabaseManager.getNomeClientes(),
+                DatabaseManager.getEnderecoClientes(), DatabaseManager.getEmailClientes(),
+                DatabaseManager.getCelularClientes(), DatabaseManager.getCpfClientes(),
+                DatabaseManager.getDtNascimentoClientes()};
+
+        db = banco.getReadableDatabase();
+
+        String condition = DatabaseManager.getIdLivros() + "=" + id;
+
+        cursor = db.query(DatabaseManager.getTabelaClientes(), campos, condition,
+                null, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public void update(int id, String nome, String endereco, String celular, String email,
+                       String cpf, String dataNascimento, String categoriaCliente) {
+
+        ContentValues valores;
+
+        db = banco.getReadableDatabase();
+
+        String condition = DatabaseManager.getIdLivros() + "=" + id;
+
+        valores = new ContentValues();
+
+        valores.put(DatabaseManager.getNomeClientes(), nome);
+        valores.put(DatabaseManager.getEnderecoClientes(), endereco);
+        valores.put(DatabaseManager.getCelularClientes(), celular);
+        valores.put(DatabaseManager.getEmailClientes(), email);
+        valores.put(DatabaseManager.getCpfClientes(), cpf);
+        valores.put(DatabaseManager.getDtNascimentoClientes(), dataNascimento);
+
+        db.update(DatabaseManager.getTabelaClientes(), valores, condition, null);
+        db.close();
+    }
+
+    public void delete(int id){
+        String condition = DatabaseManager.getIdClientes() + "=" + id;
+        db = banco.getReadableDatabase();
+        db.delete(DatabaseManager.getTabelaClientes(), condition, null);
+        db.close();
     }
 }
