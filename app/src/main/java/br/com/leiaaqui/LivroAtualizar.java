8aa5cc3ba88dde32f4473b47536cbc249e1dc152
alefreis <alef.reis@linkapi.com.br>
2019-11-03 18:39:28 -0300

    update

--- app/src/main/java/br/com/leiaaqui/LivroAtualizar.java
+++ app/src/main/java/br/com/leiaaqui/LivroAtualizar.java
@@ -2,6 +2,8 @@ package br.com.leiaaqui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,17 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
-
-public class MainActivity extends AppCompatActivity
+public class LivroAtualizar extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_livro_atualizar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,46 +32,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        Button buttonCategoriCliente = (Button) findViewById(R.id.buttonCategoriaLeitoresCRUD);
-        buttonCategoriCliente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CategoriaClienteConsulta.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        Button buttonCategoriaLivro = (Button) findViewById(R.id.buttonCategoriaLivrosCRUD);
-        buttonCategoriaLivro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CategoriaLivroConsulta.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        Button buttonCliente = (Button) findViewById(R.id.buttonLeitoresCRUD);
-        buttonCliente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClienteConsulta.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        Button buttonLivro = (Button) findViewById(R.id.buttonLivrosCRUD);
-        buttonLivro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LivroConsulta.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 
     @Override
@@ -88,7 +47,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.categoria_cliente_consulta, menu);
         return true;
     }
 
@@ -111,16 +70,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        String id = item.toString();
 
-        if (id == R.id.home) {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        Intent intent = new Intent(LivroAtualizar.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
         return true;
     }
 }
